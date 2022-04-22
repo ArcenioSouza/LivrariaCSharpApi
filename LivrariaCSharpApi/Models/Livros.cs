1280b4ca1e7eddb244319bd395adf324392f8254
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-22 16:10:16 -0300

    feat: :sparkles: Add model Livros.cs

---
+++ LivrariaCSharpApi/Models/Livros.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LivrariaCSharpApi.Models
+{
+    public class Livros
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string? Title { get; set; }
+
+        [BsonElement("author")]
+        public string? Author { get; set; }
+
+        [BsonElement("gender")]
+        public string? Gender { get; set; }
+
+        [BsonElement("qtdpages")]
+        public int QtdPages { get; set; }
+    }
+}

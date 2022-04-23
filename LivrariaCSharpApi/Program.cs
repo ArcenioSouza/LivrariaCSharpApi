04e79167b36633b41bfa1f1f06e64dd9b067785b
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-23 16:55:42 -0300

    feat: :sparkles: Add connection configuration for database access

--- LivrariaCSharpApi/Program.cs
+++ LivrariaCSharpApi/Program.cs
@@ -1,8 +1,19 @@
+using LivrariaCSharpApi.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BooksStoreDatabaseSettings>(
+    builder.Configuration.GetSection(nameof(BooksStoreDatabaseSettings)));
 
-builder.Services.AddControllers();
+builder.Services.AddSingleton<IBookStorageSettings>(sp =>
+    sp.GetRequiredService<IOptions<BooksStoreDatabaseSettings>>().Value);
+
+builder.Services.AddSingleton<IMongoClient>(s =>
+    new MongoClient(builder.Configuration.GetValue<string>("BooksStoreDatabaseSettings:ConnectionString")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

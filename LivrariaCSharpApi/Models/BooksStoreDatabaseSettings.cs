2e6a12696c9d3b083db2571fbd1e5a0e531883af
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-23 16:53:23 -0300

    feat: :sparkles: Add Model BooksStoreDatabaseSettings

---
+++ LivrariaCSharpApi/Models/BooksStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace LivrariaCSharpApi.Models
+{
+    public class BooksStoreDatabaseSettings : IBookStorageSettings
+    {
+        string IBookStorageSettings.ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IBookStorageSettings.DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IBookStorageSettings.BooksCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}

40b386bb164481fc696fd66fbf1257baa75b4e81
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-23 16:54:49 -0300

    feat: :sparkles: Add interface service IBookService

---
+++ LivrariaCSharpApi/service/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using LivrariaCSharpApi.Models;
+
+namespace LivrariaCSharpApi.service
+{
+    public interface IBookService
+    {
+        List<Livros> Get();
+        Livros Get(string id);
+        Livros Create(Livros livro);
+        void Update(string id, Livros livro);
+        void Delete(string id);
+    }
+}

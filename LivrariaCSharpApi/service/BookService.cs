1297dbe8093df44cad19813f028af43b52706cdc
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-23 16:54:26 -0300

    feat: :sparkles: Add service BookService

---
+++ LivrariaCSharpApi/service/BookService.cs
@@ -0,0 +1,41 @@
+﻿using LivrariaCSharpApi.Models;
+using MongoDB.Driver;
+
+namespace LivrariaCSharpApi.service
+{
+    public class BookService : IBookService
+    {
+        private readonly IMongoCollection<Livros> _books;
+
+        public BookService(IBookStorageSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _books = database.GetCollection<Livros>(settings.BooksCollectionName);
+        }
+        public Livros Create(Livros livro)
+        {
+            _books.InsertOne(livro);
+            return livro;
+        }
+
+        public void Delete(string id)
+        {
+            _books.DeleteOne(livro => livro.Id == id);
+        }
+
+        public List<Livros> Get()
+        {
+            return _books.Find(livro => true).ToList();
+        }
+
+        public Livros Get(string id)
+        {
+            return _books.Find(livro => livro.Id == id).FirstOrDefault();
+        }
+
+        public void Update(string id, Livros livro)
+        {
+            _books.ReplaceOne(livro => livro.Id == id, livro);
+        }
+    }
+}
